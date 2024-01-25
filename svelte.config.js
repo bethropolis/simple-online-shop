e1beccbbc61805411d3842d31e91af3159cbe580
Bethuel <66518866+bethropolis@users.noreply.github.com>
2024-01-25 14:39:19 +0000

    Add public/ directory and update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'public',
+			assets: 'public',
+			fallback: undefined,
+			precompress: false
+		})
 	},
 	preprocess: sveltePreprocess({
 		postcss: true
