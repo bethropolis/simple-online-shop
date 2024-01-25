f0fc819143e837f205acb04853c071e3ccd3352f
Bethuel <66518866+bethropolis@users.noreply.github.com>
2024-01-25 14:11:57 +0000

    Update footer and add contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,62 @@
+<div class="contact-info py-6 bg-base-100 p-10">
+    <h2 class="text-2xl font-bold mb-4">Contact Us</h2>
+    <p>Dresslagance Fashions Ltd.<br />"Retro Redefined"</p>
+    <p class="mt-2">Email: info@dresslagancefashions.com</p>
+    <p>Phone: +254 700 366783</p>
+    <div class="social mt-4">
+        <nav>
+            <h6 class="footer-title">Social</h6>
+            <div class="grid grid-flow-col gap-4">
+                <a
+                    ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        class="fill-current"
+                        ><path
+                            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+                        ></path></svg
+                    ></a
+                >
+                <a
+                    ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        class="fill-current"
+                        ><path
+                            d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
+                        /></svg
+                    ></a
+                >
+                <a
+                    ><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        class="fill-current"
+                        ><path
+                            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+                        ></path></svg
+                    ></a
+                >
+            </div>
+        </nav>
+    </div>
+</div>
+
+<style>
+    .contact-info {
+        /* Add your styles here */
+    }
+    .social {
+        width: fit-content;
+        @apply mt-4;
+    }
+    nav {
+        @apply text-neutral-700 cursor-pointer items-center;
+    }
+</style>
